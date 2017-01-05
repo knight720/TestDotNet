c467f3156beba757d5227c4704d035242cd4b169
Knight <ninku720@yahoo.com.tw>
2017-01-05 18:04:46 +0800

    密碼加密方式(小竹)

---
+++ 008密碼加密/PasswordEncode/PasswordEncode/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordEncode
+{
+    class Program
+    {
+        private static string PasswordSalt = "1qaz";
+
+        static void Main(string[] args)
+        {
+            Console.Write("Input Password: ");
+            string password = Console.ReadLine();
+
+            string encode = CryptographyPassword(password, Program.PasswordSalt);
+
+            Console.WriteLine(encode);
+
+            Console.Write("Finish!");
+            Console.ReadLine();
+        }
+
+        public static string CryptographyPassword(string password, string salt)
+        {
+            string cryptographyPassword =
+                System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(password + salt, "sha1");
+
+            return cryptographyPassword;
+        }
+
+    }
+}
