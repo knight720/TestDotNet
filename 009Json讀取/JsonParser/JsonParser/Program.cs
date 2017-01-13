31dfe1b6a33923371a98af2f8aa0b027d415e0b0
Knight <ninku720@yahoo.com.tw>
2017-01-13 15:32:41 +0800

    存取Json內的資料

---
+++ 009Json讀取/JsonParser/JsonParser/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace JsonParser
+{
+    class Program
+    {
+        static string _Url = "https://alerts.ncdr.nat.gov.tw/JSONAtomFeed.ashx?AlertType=8";
+
+        static void Main(string[] args)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.Encoding = Encoding.UTF8;
+                var json = wc.DownloadString(_Url);
+
+                Debug.WriteLine(json);
+
+                //var obj = JsonConvert.DeserializeObject(json);
+                JObject obj = JObject.Parse(json);
+
+                Console.WriteLine(obj["entry"][0]["link"]["@href"]);
+
+            }
+
+            Console.Read();
+        }
+    }
+}
