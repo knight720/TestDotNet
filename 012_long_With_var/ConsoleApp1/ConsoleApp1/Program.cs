4e4b1fd73c18b5fe8d9c170ae50ab2f93a1656db
Knight Huang <knighthuang@nine-yi.com>
2017-12-07 19:43:14 +0800

    測試var不為long的情境

---
+++ 012_long_With_var/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 100000; i++)
+            {
+                var shopId = 8;
+                object obj = (object)shopId;
+
+                var type = obj.GetType().ToString();
+                Console.WriteLine(type);
+
+                switch (type)
+                {
+                    case "System.Int32":
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+            Console.WriteLine("end...");
+            Console.Read();
+
+        }
+    }
+}
