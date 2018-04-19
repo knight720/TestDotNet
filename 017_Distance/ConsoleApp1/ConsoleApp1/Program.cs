85a267d8acdd697d8482030f9f89732264f07564
KnightHuang <knighthuang@nine-yi.com>
2018-04-19 18:31:49 +0800

    017 - 測試 System.Spatial 計算距離

---
+++ 017_Distance/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Spatial;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gp1 = GeographyPoint.Create(121.5, 23.5);
+            var gp2 = GeographyPoint.Create(121.4, 23.6);
+            var distance = GeographyOperationsExtensions.Distance(gp1, gp2);
+            Console.WriteLine($"{gp1}, {gp2}, Distance: {distance}");
+            Console.ReadLine();
+        }
+    }
+}
