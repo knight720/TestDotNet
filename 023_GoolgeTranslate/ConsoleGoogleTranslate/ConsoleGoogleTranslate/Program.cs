9e272c6298bd8bf2ce206e8876f413ce65ccb3a6
Knight Huang <knighthuang@nine-yi.com>
2018-07-06 10:17:13 +0800

    023 - 測試 google api 翻譯編碼問題 024 - 測試 TryParse 遇到 null, string.Empty 的回傳值

---
+++ 023_GoolgeTranslate/ConsoleGoogleTranslate/ConsoleGoogleTranslate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGoogleTranslate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://translate.googleapis.com/translate_a/single";
+            string sourceLanguage = "zh-TW";
+            string targetLanguage = "en";
+            string text = "中文";
+
+            string fullUrl = $"{url}?client=gtx&sl={sourceLanguage}&tl={targetLanguage}&dt=t&q={text}";
+
+            WebClient webClient = new WebClient();
+            webClient.Encoding = Encoding.GetEncoding(950);
+            string responce = webClient.DownloadString(fullUrl);
+
+            Console.WriteLine(responce);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 024_TryParse/TestTryParse/TestTryParse/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isEnable = true;
+            // null > False
+            // string.Empty > False
+            // "false" or "False" > Fasle 
+            // "aalse" > Fasle 
+            // "true" or "True" or "TrUe" > True
+            string strIsEnable = "TrUe";
+            bool.TryParse(strIsEnable, out isEnable);
+
+            Console.WriteLine(isEnable);
+
+            Console.ReadLine();
+        }
+    }
+}
