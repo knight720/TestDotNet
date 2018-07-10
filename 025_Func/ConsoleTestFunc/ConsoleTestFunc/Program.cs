b310e8f3aa59beb5c05534af255ad7782a2666f9
Knight Huang <knighthuang@nine-yi.com>
2018-07-10 12:02:51 +0800

    025 - 修正顯示內容

--- 025_Func/ConsoleTestFunc/ConsoleTestFunc/Program.cs
+++ 025_Func/ConsoleTestFunc/ConsoleTestFunc/Program.cs
@@ -9,25 +9,45 @@ namespace ConsoleTestFunc
             var p = new Program();
 
             var result = p.GetRData("rKey");
-            //Console.WriteLine(result);
+            Console.WriteLine(result);
 
             Console.WriteLine("===");
-
             result = p.GetMData("mKey");
-            //Console.WriteLine(result);
-            
+            Console.WriteLine(result);
+
+            Console.WriteLine("===");
+            result = p.GetRData("rKey");
+            Console.WriteLine(result);
+
+            Console.WriteLine("===");
+            result = p.GetMData("mKey");
+            Console.WriteLine(result);
+
+            Console.WriteLine("===");
+            result = p.GetRData("rKey", true);
+            Console.WriteLine(result);
+
+            Console.WriteLine("===");
+            result = p.GetMData("mKey", true);
+            Console.WriteLine(result);
+
             Console.ReadLine();
         }
 
         private string _mCache;
         private string _rCache;
 
-        private string GetMData(string key)
+        private string GetMData(string key, bool clear = false)
         {
             Func<string> getDataFunc = () =>
             {
                 return this.GetRData(key);
             };
+
+            if (clear)
+            {
+                this._mCache = null;
+            }
 
             //// key 加入 locale
             key = this.AppendLocale(key);
@@ -43,28 +63,33 @@ namespace ConsoleTestFunc
                 result = this._mCache;
             }
 
-            Console.WriteLine($"GetMDate: {key}");
+            Console.WriteLine($"GetMDate Key: {key}");
 
             return result;
         }
 
-        private string GetRData(string key)
+        private string GetRData(string key, bool clear = false)
         {
             //// key 加入 locale
             key = this.AppendLocale(key);
 
-            var result = string.Empty;
-            if (this._rCache != null)
+            if (clear)
             {
-                result = this._rCache;
+                this._rCache = null;
+            }
+
+            var result = string.Empty;
+            if (this._rCache == null)
+            {
+                result = $"RData: {DateTime.Now.Millisecond}";
+                this._rCache = result;
             }
             else
             {
-                this._rCache = key;
-                result = key;
+                result = this._rCache;
             }
 
-            Console.WriteLine($"GetRDate: {key}");
+            Console.WriteLine($"GetRDate Key: {key}");
 
             return result;
         }
