f3e9086cdb3e47e1d387d074fb3f1fb0bdc640e1
Knight Huang <knighthuang@91app.com>
2018-12-20 13:51:47 +0800

    030 - Contains 效能比較

---
+++ 030_Contains_Perfomance/ConsoleApp1/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int _MaxTimes = 999999;
+        static int _ListCount = 2;
+
+        static void Main(string[] args)
+        {
+            Stopwatch watch = Stopwatch.StartNew();
+            for (int i = 0; i < _MaxTimes; i++)
+            {
+                Method1();
+            }
+            watch.Stop();
+            Console.WriteLine("General: {0:N0}ms", watch.ElapsedMilliseconds);
+
+            watch = Stopwatch.StartNew();
+            for (int i = 0; i < _MaxTimes; i++)
+            {
+                Method2();
+            }
+            watch.Stop();
+            Console.WriteLine("List: {0:N0}ms", watch.ElapsedMilliseconds);
+
+            watch = Stopwatch.StartNew();
+            for (int i = 0; i < _MaxTimes; i++)
+            {
+                Method3();
+            }
+            watch.Stop();
+            Console.WriteLine("List ot Array: {0:N0}ms", watch.ElapsedMilliseconds);
+
+            watch = Stopwatch.StartNew();
+            for (int i = 0; i < _MaxTimes; i++)
+            {
+                Method4();
+            }
+            watch.Stop();
+            Console.WriteLine("HasSet: {0:N0}ms", watch.ElapsedMilliseconds);
+
+            Console.ReadLine();
+        }
+
+        static void Method1()
+        {
+            var input = Generate();
+            if (input != Generate())
+            {
+                // Do Nothing
+            }
+        }
+
+        static void Method2()
+        {
+            var inputList = new List<string>();
+            inputList.Add(Generate());
+
+            for (int i = 0; i< _ListCount;i++)
+            {
+                if ("area" == "area")
+                {
+                    inputList.Add(Generate());
+                }
+            }
+
+            if (inputList.Contains(Generate()))
+            {
+                // Do Nothing
+            }
+        }
+
+        static void Method3()
+        {
+            var inputList = new List<string>();
+            inputList.Add(Generate());
+
+            for (int i = 0; i < _ListCount; i++)
+            {
+                if ("area" == "area")
+                {
+                    inputList.Add(Generate());
+                }
+            }
+
+            var inputArray = inputList.ToArray();
+            if (inputArray.Contains(Generate()))
+            {
+                // Do Nothing
+            }
+        }
+
+        static void Method4()
+        {
+            var inputSet = new HashSet<string>();
+            inputSet.Add(Generate());
+
+            for (int i = 0; i < _ListCount; i++)
+            {
+                if ("area" == "area")
+                {
+                    inputSet.Add(Generate());
+                }
+            }
+
+            if (inputSet.Contains(Generate()))
+            {
+                // Do Nothing
+            }
+        }
+
+
+        static string Generate()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ 030_Contains_Perfomance/README.md
@@ -0,0 +1,22 @@
+# Result
+
+_MaxTimes = 999999;  
+_ListCount = 2;   
+
+General: 1,456ms  
+List: 2,800ms  
+List ot Array: 2,932ms  
+HasSet: 3,058ms  
+
+---  
+_MaxTimes = 99999;  
+_ListCount = 99;  
+
+General: 165ms  
+List: 6,945ms  
+List ot Array: 6,835ms  
+HasSet: 7,488ms  
+
+# Reference
+[Performance Benchmarking of Contains, Exists and Any
+](https://stackoverflow.com/questions/18651940/performance-benchmarking-of-contains-exists-and-any/34920719)
