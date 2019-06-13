e7d9b129f950cd8ac1961bbc51a21b4431a003b5
Knight Huang <knighthuang@91app.com>
2019-06-13 18:21:56 +0800

    035 - Inner 與 Outer 皆為 zh-TW

---
+++ 035_CurrentUICulture/WebApplication1/Controllers/Apis/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web.Http;
+using Newtonsoft.Json;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers.Apis
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/<controller>
+        public string Get()
+        {
+            CultureInfo.CurrentUICulture = CultureInfo.CreateSpecificCulture("zh-TW");
+
+            IList<string> list = new List<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(CultureInfo.CurrentUICulture.Name);
+            }
+
+            var result = list.Select(i =>
+                new LocaleEntity()
+                {
+                    OuterLocale = i
+                }
+            ).ToList();
+
+            return JsonConvert.SerializeObject(result);
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ 035_CurrentUICulture/WebApplication1/Models/LocaleEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace WebApplication1.Models
+{
+    public class LocaleEntity
+    {
+        public string InnerLocale
+        {
+            get { return CultureInfo.CurrentUICulture.Name; }
+        }
+
+        public string OuterLocale { get; set; }
+    }
+}
